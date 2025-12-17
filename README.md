fdbef8d86dae289755127748f765a631a34d8504
votre_nom <sabiressaad24@gmail.com>
2025-12-17 19:23:36 +0100

    Initial project push

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # TP-31-Microservices-Spring-Boot-avec-RabbitMQ-messagerie-JSON-persistance-MySQL-
+=======
+
+# TP 31 : Microservices Spring Boot avec RabbitMQ (messagerie JSON + persistance MySQL)
+>>>>>>> 3a7a503 (Initial project push)
---
+++ spring-rabbitmq-consumer/src/main/java/com/oussama/rabbitmicro/SpringRabbitmqConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.oussama.rabbitmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqConsumerApplication.class, args);
+    }
+}
